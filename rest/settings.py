c1ceeffcee88184b56c00b1db31d2eebec8c5170
todi-2000 <manshitodi@gmail.com>
2020-07-03 12:55:33 +0530

    Changed Debug mode

--- rest/settings.py
+++ rest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(rt!x_k!ps#^ns&d#9rie&gg1!$mzcsd53^2bmql!1_t$2tcz&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
